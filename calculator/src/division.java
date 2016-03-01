0ee71d62c45bbfb0ed1e12509000b1db441c96d2
alumno <alumno@aula01-07>
2016-03-01 17:46:01 +0100

    clase division

---
+++ calculator/src/division.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alumno
+ */
+public class division {
+    
+    public double Division(int num1, int num2){
+     double resul=0;
+     resul=num1/num2; 
+     return resul;
+ }
+}
