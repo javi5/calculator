a19b122ca71c374e780179bf437216c9da234259
AlbertoCY <infoalbeto@gmail.com>
2016-03-08 17:53:29 +0100

    Mejorada

--- calculator/src/division.java
+++ calculator/src/division.java
@@ -12,7 +12,9 @@ public class division {
     
     public double Division(int num1, int num2){
      double resul=0;
-     resul=num1/num2; 
+     if(num1 != 0 && num2 != 0){
+        resul=num1/num2;
+     }
      return resul;
  }
     
